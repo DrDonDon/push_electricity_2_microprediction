9dfa3f45f10b9a5306939c9868fea71da14d4a45
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-06-12 13:49:02 +1000

    Update to write key

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ client = AmphoraDataRepositoryClient(credentials)
 
 SA_id = '89c2e30d-78c8-46ef-b591-140edd84ddb6'
 
-mw = MicroWriter(write_key="89c2e30d-78c8-46ef-b591-140edd84ddb")
+mw = MicroWriter(write_key="2d6029b15b6e49ae1d78dec4b551bdd")
 
 amphora = client.get_amphora(SA_id)
 signals = amphora.get_signals()
