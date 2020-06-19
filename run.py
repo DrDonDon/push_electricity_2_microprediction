d1568fd25f53b11e68090855bf66f5f2141d1088
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-06-19 13:12:39 +1000

    Update to be clearer on write time

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from amphora.client import AmphoraDataRepositoryClient, Credentials
 from microprediction import MicroWriter
 import time
-import os   
+import os
+from datetime import datetime, timedelta
+import amphora_api_client as a10a
 
 credentials = Credentials(username=os.getenv('username'), password=os.getenv('password'))
 client = AmphoraDataRepositoryClient(credentials) 
@@ -10,9 +12,12 @@ SA_id = '89c2e30d-78c8-46ef-b591-140edd84ddb6'
 
 mw = MicroWriter(write_key="bdfd44affd28e6c5b45329d6d4df7729")
 
-amphora = client.get_amphora(SA_id)
+time_range = a10a.DateTimeRange(_from = datetime.utcnow() + timedelta(hours=-1) , to= datetime.utcnow() )
+
+amphora = client.get_amphora(Amphora_id)
 signals = amphora.get_signals()
-df = signals.pull().to_pandas()
+df = signals.pull(date_time_range=time_range).to_pandas()
+
 price = df['price']
-print(price[-1])
+
 mw.set(name='South_Australia_Electricity_Price.json',value=price[-1])
