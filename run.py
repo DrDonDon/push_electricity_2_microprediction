4042e8e255e3c7bfb207f6cc0c0e3ecea41b901d
DrDonDon <53420422+DrDonDon@users.noreply.github.com>
2020-07-24 10:51:43 +1000

    Update to run model live

--- run.py
+++ run.py
@@ -7,31 +7,51 @@ import amphora_api_client as a10a
 import numpy as np
 import itertools
 import statsmodels.api as sm
+from statsmodels.tsa.vector_ar.var_model import VAR
 
 credentials = Credentials(username=os.getenv('username'), password=os.getenv('password'))
 client = AmphoraDataRepositoryClient(credentials) 
 
-Amphora_id = '89c2e30d-78c8-46ef-b591-140edd84ddb6'
+Electricity_Amphora_id = '89c2e30d-78c8-46ef-b591-140edd84ddb6'
+Weather_Amphora_id = '916058ba-cb52-40f6-8cf2-c4aafeb4c26e'
 name = 'South_Australia_Electricity_Price.json'
 
 # Upload data
 mw = MicroWriter(write_key="bdfd44affd28e6c5b45329d6d4df7729")
 time_range = a10a.DateTimeRange(_from = datetime.utcnow() + timedelta(hours=-1) , to= datetime.utcnow() )
-amphora = client.get_amphora(Amphora_id)
-signals = amphora.get_signals()
-df = signals.pull(date_time_range=time_range).to_pandas()
+electricity_amphora = client.get_amphora(Electricity_Amphora_id)
+electricity_signals = electricity_amphora.get_signals()
+df = electricity_signals.pull(date_time_range=time_range).to_pandas()
 price = df['price']
 mw.set(name=name,value=price[-1])
 
+print(price)
+
 # Do prediction for next value
 length = mw.num_predictions
-prev_data = mw.get_lagged_values(name=name)
-prev_data = np.array(prev_data[0:101])
-current_val = prev_data[0]
 
-model_fit = sm.load('predictive_model.pickle')
-std_val = 8.1 # estimated previously
+# pull data to do model
+data_time_range = a10a.DateTimeRange(_from = datetime.utcnow() + timedelta(hours=-length/2) , to= datetime.utcnow() )
+pdf = electricity_signals.pull(date_time_range=data_time_range).to_pandas()
+price = pdf['price']
+prices_of_interest = price[0:length]
+
+elec_prices_diff = np.zeros(length-1)
+for t in range(1,length):
+    elec_prices_diff[t-1] = prices_of_interest[t] - prices_of_interest[t-1]
+std_val = np.std(elec_prices_diff[abs(elec_prices_diff)<30])
+
+weather_amphora = client.get_amphora(Weather_Amphora_id)
+weather_signals = weather_amphora.get_signals()
+wdf = weather_signals.pull(date_time_range=data_time_range).to_pandas()
+data_of_interest = wdf[0:length]
+
+training_data = np.transpose(np.array([prices_of_interest, data_of_interest['pressure'], data_of_interest['airTemp']]))
+
+model = VAR(training_data)
+model_fit = model.fit()
 
 yhat = model_fit.forecast(model_fit.y, steps=1)
+
 dist = np.random.laplace(yhat[0,0], std_val/np.sqrt(2), length)
 res = mw.submit(name=name, values=dist, delay=None, verbose=None)
